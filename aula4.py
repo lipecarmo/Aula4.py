aa1297069ca526b36f7fe6bdf104af1fe2421660
lipecarmo <lipeoliveira523@gmail.com>
2024-10-14 20:42:30 -0300

    Update aula4.py

--- aula4.py
+++ aula4.py
@@ -1,21 +1,21 @@
-# print(list(range(5,16,3)))
+print(list(range(5,16,3)))
 
-# for i in range(10):
-#     print(i*5)
-
-
-# tabuada=int(input('fale um numero:'))
-
-# for i in range(1,11):
-#     print(f'{tabuada} x {i} = {i*tabuada}')
+for i in range(10):
+    print(i*5)
 
 
-# soma = 0 
-# for i in range(1,101):
-#     print(f'{soma} + {i} = {soma + i}')
-#     soma =+ i
+tabuada=int(input('fale um numero:'))
 
-# print(f'O resultado da soma dos numeros de 1 ate 100 é:{soma}')
+for i in range(1,11):
+    print(f'{tabuada} x {i} = {i*tabuada}')
+
+
+soma = 0 
+for i in range(1,101):
+    print(f'{soma} + {i} = {soma + i}')
+    soma =+ i
+
+print(f'O resultado da soma dos numeros de 1 ate 100 é:{soma}')
 
 # Atividade 03:
 # Caractere por Caractere:
@@ -23,14 +23,14 @@
 # laço for para exibir cada caractere da palavra em uma linha separada.
 
 
-# palavra = input('Digite uma plavra:')
-# for letra in palavra:
-#     print(letra)
+palavra = input('Digite uma plavra:')
+for letra in palavra:
+    print(letra)
 
-# for i in range(10,0,-1):
-#     print(i)
+for i in range(10,0,-1):
+    print(i)
 
-# print('Feliz ano novo')
+print('Feliz ano novo')
 
 
 # Atividade 07:
@@ -38,15 +38,15 @@
 # Crie um programa que solicite uma palavra ao usuário e use um laço for com
 # uma condicional para contar quantas vogais (a, e, i, o, u) a palavra contém.
 
-# texto = 'Progamação em Python'
+texto = 'Progamação em Python'
 
-# qtd__vogais = 0
+qtd__vogais = 0
 
-# for letra in texto:
-#     if letra.lower() in 'aeiou':
-#         qtd__vogais += 1
+for letra in texto:
+    if letra.lower() in 'aeiou':
+        qtd__vogais += 1
 
-# print(f'Existem {qtd__vogais} vogais no texto.')
+print(f'Existem {qtd__vogais} vogais no texto.')
 
 # Atividade 05:
 # Contagem de Números Positivos e Negativos:
@@ -54,18 +54,18 @@
 # laço for com uma condicional para contar quantos são positivos e
 # quantos são negativos.
 
-# numeros_positivos = 0
-# numeros_negativos = 0
+numeros_positivos = 0
+numeros_negativos = 0
 
-# for i in range(10):
-#     numero = int(input('Digite um numero:'))
-#     if numero > 0:
-#         numeros_positivos += 1
-#     elif numero < 0:
-#         numeros_negativos +=1
+for i in range(10):
+    numero = int(input('Digite um numero:'))
+    if numero > 0:
+        numeros_positivos += 1
+    elif numero < 0:
+        numeros_negativos +=1
 
-# print(f'Total de numeros positivos digitados: {numeros_positivos}')
-# print(f'Total de numeros negativos digitados: {numeros_negativos}')
+print(f'Total de numeros positivos digitados: {numeros_positivos}')
+print(f'Total de numeros negativos digitados: {numeros_negativos}')
 
 
 # Escreva um programa que use um laço for para somar
@@ -73,21 +73,21 @@
 # Escreva um programa que use um laço for para somar
 # todos os números pares de 1 a 50 e exiba o resultado final.
 
-# qtd_pares = 0
-# for i in range(1,51):
-#     if i % 2 == 0:
-#         qtd_pares += i
+qtd_pares = 0
+for i in range(1,51):
+    if i % 2 == 0:
+        qtd_pares += i
 
-# print(qtd_pares)
+print(qtd_pares)
 
 # Atividade 08:
 # Calculo de Média de Notas:
 # Escreva um programa que use um laço for para somar
 # todos os números pares de 1 a 50 e exiba o resultado final.
 
-# soma_notas = 0
-# for i in range (5):
-#     nota = float()
+soma_notas = 0
+for i in range (5):
+    nota = float()
 
 
 # Atividade 09:
@@ -95,13 +95,13 @@
 # Crie um programa que use um laço for para contar de 1 a 20. Use condicionais para
 # identificar números pares e ímpares. Pare o loop ao encontrar o número 15, usando break.
 
-# for i in range(1,21):
-#     if i == 15:
-#         break
-#     if i % 2 == 0:
-#         print(f'{i} - Par')
-#     else:
-#         print (f'{i} - Impar' ) 
+for i in range(1,21):
+    if i == 15:
+        break
+    if i % 2 == 0:
+        print(f'{i} - Par')
+    else:
+        print (f'{i} - Impar' ) 
 
 
 
